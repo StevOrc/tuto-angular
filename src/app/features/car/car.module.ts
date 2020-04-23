8dc7cc4c39c648c1ec180af1e0ca699c7043011f
StevOrc <steven.orcun2@gmail.com>
2020-04-23 20:02:51 +0200

    archi ok

---
+++ src/app/features/car/car.module.ts
@@ -0,0 +1,16 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { CarListComponent } from './car-list/car-list.component';
+
+
+
+@NgModule({
+  declarations: [CarListComponent],
+  imports: [
+    CommonModule
+  ],
+  exports: [
+    CarListComponent
+  ]
+})
+export class CarModule { }
